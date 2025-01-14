413508e56efc6aae8670b5b1b81ea9517f4b5d04
Miki2531 <mikiyasshemelis46@gmail.com>
2025-01-14 07:26:55 +0300

    Update date range in SQL query for active users count

--- #21_days _of_SQL_challenges/day_8.sql
+++ #21_days _of_SQL_challenges/day_8.sql
@@ -20,6 +20,6 @@ Return the result table in any order.*/
 
 SELECT activity_date AS days, COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE activity_date BETWEEN '2019-07-01' AND '2019-07-27'
+WHERE activity_date > '2019-06-27' AND activity_date <= '2019-07-27'
 GROUP BY activity_date
 ORDER BY activity_date;
