eadd23e2caaec4863eddfcba2623ff85008237a6
Miki2531 <mikiyasshemelis46@gmail.com>
2025-01-23 11:44:23 +0300

    Refactor SQL query to remove duplicate emails from Person table using a common table expression

--- #21_days _of_SQL_challenges/day_16.sql
+++ #21_days _of_SQL_challenges/day_16.sql
@@ -20,14 +20,7 @@ For Pandas users, please note that you are supposed to modify Person in place.
 After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.
 */
 
-/*Solution 1*/
-DELETE p1
-FROM Person p1
-JOIN Person p2
-ON p1.email = p2.email
-AND p1.id > p2.id;
-
-/*Solution 2*/
+/*Solution*/
 WITH duplicates AS (
     SELECT MIN(id) AS email_duplicate
     FROM Person
